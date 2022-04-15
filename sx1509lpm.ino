5f0f3dd729276c157ed35b1a34a7fb5a92ba3159
stivb <s.j.bennett@herts.ac.uk>
2022-04-15 22:25:16 +0100

    Nice progress on setting the bars

--- sx1509lpm.ino
+++ sx1509lpm.ino
@@ -8,8 +8,6 @@
 #include <Smoothed.h> 
 
 
-
-
 #include "LedControl.h"
 #define LED_DIN 10
 #define LED_CS 16
@@ -17,10 +15,10 @@
 #define NPOTS 4
 #define CHN 15
 #define DRUMCHAN 9
-
-
 LedControl lc=LedControl(LED_DIN,LED_CLK,LED_CS,1);
-int delaytime=300;
+
+
+//int delaytime=300;
 
 // SX1509 I2C address (set by ADDR1 and ADDR0 (00 by default):
 const byte SX1509_ADDRESS = 0x3E; // SX1509 I2C address
@@ -51,7 +49,7 @@ bool noPots = false;
 const byte maxPins=16;
 const int slideButtonLevels[3] = {81,99,68};
 
-int constCt=0;
+unsigned long constCt=0;
 
 
 
@@ -60,32 +58,12 @@ Smoothed <int> Sensors[3];
 int sensorCt = 3;
 int aPins[3] = {A3,A1,A2};
 
-Smoothed <int> sliderSensor;
+//Smoothed <int> sliderSensor;
 
 boolean isIn(int array[], int element) {
  for (int i = 0; i < 4; i++) if (array[i] == element) return true;
  return false;
 }
-
-int getNearestPosition(int val)
-{
-  int distance = 128;
-  int nearest = 0;
-  int i;
-  for(i=0;i<3;i++)
-  {
-    int tempdist = abs(val-slideButtonLevels[i]);
-    if (tempdist<distance)
-    {
-      distance = tempdist;
-      nearest = i;
-    }
-  }
-  Serial.print("Nearest is ");Serial.print(nearest);Serial.print(" Val is ");Serial.println(val);
-  return pow(2,(nearest+1));
-}
-
-
 
 byte lastVal[maxPins] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
 
@@ -114,11 +92,17 @@ void setup()
   for (int i=0;i<maxPins;i++) io.pinMode(i, INPUT_PULLUP);
 
   for (int i=0;i<3;i++) Sensors[i].begin(SMOOTHED_AVERAGE,8);
-  sliderSensor.begin(SMOOTHED_AVERAGE,10);
+  //sliderSensor.begin(SMOOTHED_AVERAGE,10);
 
   setUpLED();
   hello();
+  doDefaults();
   
+  
+}
+
+void doDefaults()
+{
   
 }
 
@@ -132,20 +116,10 @@ void setUpLED()
 
 void loop()
 {
-
-  
-  //Serial.println("hello");
-
   readDrumButtons();
   readFxButtons();
-  //readButtons();
   rPots();
-  //readSlider();
   readSlide();
-
-  
-
-  
 }
 
 void readDrumButtons()
@@ -183,77 +157,35 @@ void readFxButtons()
   if (hasChanged) {delay(50);hasChanged=false;}
 }
 
-void readButtons()
-{
-  for (byte i=0;i<maxPins;i++)
-  {
-    int btnState = io.digitalRead(i);
 
-    if (btnState!=lastVal[i]) 
-    {
-      lastVal[i]=btnState;
-      hasChanged=true;
-      //if (debugging) {Serial.print("Button "); Serial.println(i); Serial.print(":");Serial.println(btnState);}
-      if (isIn(drumBtns,i))
-      {
-        if (debugging) {Serial.print("note on note off for "); Serial.println(i);}
-        if (btnState==HIGH)  noteOn(DRUMCHAN,0x24+i,0xD0);
-        else noteOff(DRUMCHAN,0x24+i,0xD0);
-        MidiUSB.flush();
-      }
-      else
-      {
-        if (debugging) {Serial.print("control for"); Serial.println(i);}
-        if (btnState==HIGH)  controlChange(0x00,0x30+i,0x1);
-        else controlChange(CHN,0x30+i,0x60);
-        MidiUSB.flush();
-      }
-    } 
-    
-  }
-  if (hasChanged) {delay(50);hasChanged=false;}
+int qTize(int num, int arr[], int arr_size)
+{
+
+int retval = arr[0];
+int dx = abs(arr[0]-num);
+
+for(int i = 1;i < arr_size; i++)
+{
+   int tmpDx = abs(arr[i]-num);   
+   
+   if (tmpDx<dx)
+   {
+    dx = tmpDx;
+    retval = arr[i];
+   }
+}
+
+return retval;
+
 }
 
 
-bool diff(int a, int b)
-{
-  if (abs(a-b)>3) return true;
-  return false;
-}
 
-
-void readSlider()
-{
-  
-  sliderSensor.add(analogRead(A0)/60);
-  int slideVal = quantize(sliderSensor.get());
-  
-  if (slideVal!=lastSlideVal )   makeChange(slideVal);
-  
- 
-  
-}
-
-void makeChange(int sv)
-{
-  if (millis()-pendedAt <1000) return;
-  if (debugging) Serial.print("The value is ");
-  if (debugging) Serial.println(sv);
-  pendedAt = millis();
-  lastSlideVal=sv;
-}
 
 int quantize(int sv)
 {
-  
-  for (int i=1;i<4;i++)
-  {
-    if (sv<=i*2)
-    {
-      return i;
-    }
-  }
-  
+for (int i=1;i<4;i++) if (sv<=i*2) return i;
+return 0;
 }
 
 
@@ -267,7 +199,7 @@ void rPots()
     {
       Sensors[i].add(analogRead(aPins[i]));
       val= round(Sensors[i].get()/(float)8);
-      //if (aPins[i]==A0) val= getNearestPosition(val);
+      
       if (abs(val-lastPotVals[i])>3) 
         {
         hasChanged=true;
@@ -275,7 +207,7 @@ void rPots()
         printPotByPos(val,i);
         if (!noPots)
           {
-          controlChange(CHN,60+i,128-val);
+          if (i==0) controlChange(CHN,60+i,(128-val)/2); else controlChange(CHN,60+i,128-val);
           MidiUSB.flush();
           }
         }
@@ -289,21 +221,7 @@ void rPots()
 
 
 
-void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
-}
 
-void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
-}
-
-void controlChange(byte channel, byte control, byte value) {
-  if (debugging) {Serial.print(control);Serial.print(":");Serial.println(value);}
-  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
-  MidiUSB.sendMIDI(event);
-}
 
 void printPotByPos(byte num,byte pos)
 {
@@ -326,6 +244,64 @@ void printDblDigitAt(byte nm, byte pos)
   }
 }
 
+
+
+
+
+
+void readSlide()
+{
+  int reading = quantize(round(analogRead(A0)/(float)50));
+  if (reading!=lastSlideVal) 
+  {
+    lastSlideVal=reading;   
+    valueAnnounced=false;
+    constCt=0;
+  }    
+  else
+    {
+      constCt++;
+      if (constCt>20 && !valueAnnounced)
+      
+      {
+        if (debugging) Serial.print("New Val is ");
+        if (debugging) Serial.println(reading);
+        constCt=0;
+        valueAnnounced=true;
+        int bars = round(pow(2,reading));
+        if (debugging) Serial.println(bars);
+        if (bars!=lastBars)
+          {
+          int possValues[] = {12,28,60};
+          int val2BeSent = qTize(bars*16,possValues,3);
+          controlChange(CHN,70,val2BeSent);
+          printDblDigitAt(val2BeSent,3);
+          MidiUSB.flush();
+          lastBars = bars;
+          }
+        
+      }
+    }
+  
+}
+
+
+
+void noteOn(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOn);
+}
+
+void noteOff(byte channel, byte pitch, byte velocity) {
+  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+  MidiUSB.sendMIDI(noteOff);
+}
+
+void controlChange(byte channel, byte control, byte value) {
+  if (debugging) {Serial.print(control);Serial.print(":");Serial.println(value);}
+  midiEventPacket_t event = {0x0B, 0xB0 | channel, control, value};
+  MidiUSB.sendMIDI(event);
+}
 
 
 
@@ -365,50 +341,22 @@ void hello(){
   
 }
 
-void readSlide()
-{
-  int reading = quantize(round(analogRead(A0)/(float)50));
-  if (reading!=lastSlideVal) 
-  {
-    if (reading==2 && lastSlideVal==1)
-    {
-      noPots=true;
-    }
 
-    if (reading==1 && lastSlideVal==2)
-    {
-      noPots=false;
-    }
-    lastSlideVal=reading;   
-    valueAnnounced=false;
-    
-  }    
-  else
-    {
-      constCt++;
-      if (constCt>20 && !valueAnnounced)
-      {
-        if (debugging) Serial.print("New Val is ");
-        if (debugging) Serial.println(reading);
-        constCt=0;
-        valueAnnounced=true;
-        int bars = round(pow(2,reading));
-        if (debugging) Serial.println(bars);
-        if (bars!=lastBars)
-          {
-          controlChange(CHN,70,bars);
-          MidiUSB.flush();
-          lastBars = bars;
-          }
-        printDblDigitAt(bars,3);
-      }
-    }
-  
-}
 
-void handleRotation()
-{
-  
+//void makeChange(int sv)
+//{
+//  if (millis()-pendedAt <1000) return;
+//  if (debugging) Serial.print("The value is ");
+//  if (debugging) Serial.println(sv);
+//  pendedAt = millis();
+//  lastSlideVal=sv;
+//}
+
+
+
+//void handleRotation()
+//{
+//  
 //  bool vPotChanged=false;
 //  bool tPotChanged=false;
 //  tPotPos = tPot->getVal();
@@ -433,27 +381,70 @@ void handleRotation()
 //    Serial.print("V Pot Val: ");
 //    Serial.println(vPotPos);
 //  }
-}
+//}
 
-void readPots()
-{
-  bool hasChanged=false;
-  for (int i=0;i<3;i++)
-  {
-    int val = analogRead(aPins[i])/8;
-    if (val!=lastPotVals[i]) 
-      {
-        //controlChange(0x00,0x40+i,0x7F-val);
-        hasChanged=true;
-        lastPotVals[i]=val;
-        //printNumToLed(val,i-A2);
-        printPotByPos(val,i);
-        }
-    
-  }
-  if (hasChanged)  
-  {
-    delay(5);
-    //MidiUSB.flush();
-  }
-}
+
+//bool diff(int a, int b)
+//{
+//  if (abs(a-b)>3) return true;
+//  return false;
+//}
+//
+//
+//void readSlider()
+//{
+//  sliderSensor.add(analogRead(A0)/60);
+//  int slideVal = quantize(sliderSensor.get());
+//  
+//  if (slideVal!=lastSlideVal )   makeChange(slideVal);
+//}
+//
+//void readButtons()
+//{
+//  for (byte i=0;i<maxPins;i++)
+//  {
+//    int btnState = io.digitalRead(i);
+//
+//    if (btnState!=lastVal[i]) 
+//    {
+//      lastVal[i]=btnState;
+//      hasChanged=true;
+//      //if (debugging) {Serial.print("Button "); Serial.println(i); Serial.print(":");Serial.println(btnState);}
+//      if (isIn(drumBtns,i))
+//      {
+//        if (debugging) {Serial.print("note on note off for "); Serial.println(i);}
+//        if (btnState==HIGH)  noteOn(DRUMCHAN,0x24+i,0xD0);
+//        else noteOff(DRUMCHAN,0x24+i,0xD0);
+//        MidiUSB.flush();
+//      }
+//      else
+//      {
+//        if (debugging) {Serial.print("control for"); Serial.println(i);}
+//        if (btnState==HIGH)  controlChange(0x00,0x30+i,0x1);
+//        else controlChange(CHN,0x30+i,0x60);
+//        MidiUSB.flush();
+//      }
+//    } 
+//    
+//  }
+//  if (hasChanged) {delay(50);hasChanged=false;}
+//}
+
+
+//int getNearestPosition(int val)
+//{
+//  int distance = 128;
+//  int nearest = 0;
+//  int i;
+//  for(i=0;i<3;i++)
+//  {
+//    int tempdist = abs(val-slideButtonLevels[i]);
+//    if (tempdist<distance)
+//    {
+//      distance = tempdist;
+//      nearest = i;
+//    }
+//  }
+//  Serial.print("Nearest is ");Serial.print(nearest);Serial.print(" Val is ");Serial.println(val);
+//  return pow(2,(nearest+1));
+//}
